37cf6f9d3f22f68bd3d16276714cb168c70b04a5
nchevrier <nicolaschevrier@live.fr>
2019-04-11 16:10:28 +0200

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "212.47.229.152:39503",
+  "51.158.98.168:39503",
   //"your_seed_ip2.com:8080",
 };
 
